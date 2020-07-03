2e107720bb647441a5ba96fcee2b3eff3358af03
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-03 16:42:37 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,15 @@
 class Application
+
     @@items = []
+
     def call(env)
+
        resp = Rack::Response.new
        req = Rack::Request.new(env) 
 
        if req.path.match(/items/)
         
-        item_name = req.path.match("/items/").last
+        item_name = req.path.split("/items/").last
 
         item = @@items.find{|i| i.name == item_name}
 
@@ -16,6 +19,7 @@ class Application
         else
             resp.write item.price
         end
+
         else
             resp.write "Route not found"
             resp.status = 404
